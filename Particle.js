18674f2945542a849afeb69492798b9136a1f5f1
Nadish25 <75306433+Nadish25@users.noreply.github.com>
2021-02-09 21:04:33 +0530

    Add files via upload

--- Particle.js
+++ Particle.js
@@ -8,7 +8,7 @@ class Particle {
       
         this.body = Matter.Bodies.circle(x, y, this.r,options);       
         this.color=color(random(0, 255), random(0, 255), random(0, 255));
-        World.add(world, this.body);
+        Matter.World.add(world, this.body);
 
     }
     display() {
